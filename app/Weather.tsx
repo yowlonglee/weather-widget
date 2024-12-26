48d7b10d7f621e4c213410dcd2c8f8de8efa34de
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2024-12-26 15:41:46 +0800

    Remove date

--- app/Weather.tsx
+++ app/Weather.tsx
@@ -3,10 +3,8 @@ import fetchJsonp from "fetch-jsonp";
 import { useState } from "react";
 export default function Weather() {
   const [weather, setWeather] = useState();
-  const today = new Date();
-  const url = `https://cts.com.tw/api/weather_list.json?t=${
-    today.getMonth() + 1
-  }${today.getDate()}`;
+  // const today = new Date();
+  const url = "https://cts.com.tw/api/weather_list.json";
   fetchJsonp(url, { jsonpCallbackFunction: "callback_weather" })
     .then(function (response) {
       return response.json();
