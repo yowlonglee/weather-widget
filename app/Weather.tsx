af07784a493016f789c90b93c665925b2b29c534
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2024-12-27 11:44:18 +0800

    Add weather condition

--- app/Weather.tsx
+++ app/Weather.tsx
@@ -1,8 +1,8 @@
 "use client";
 import fetchJsonp from "fetch-jsonp";
-// import { useState } from "react";
+import { useState } from "react";
 export default function Weather() {
-  // const [weather, setWeather] = useState();
+  const [weather, setWeather] = useState();
   const today = new Date();
   const url = `https://www.cts.com.tw/api/weather_list.json?t=${
     today.getMonth() + 1
@@ -13,10 +13,11 @@ export default function Weather() {
       return response.json();
     })
     .then(function (json) {
+      setWeather(json[18].state);
       console.log("parsed json", json);
     })
     .catch(function (ex) {
       console.log("parsing failed", ex);
     });
-  return <div className="flex justify-center">weather</div>;
+  return <div className="flex justify-center">{weather}</div>;
 }
