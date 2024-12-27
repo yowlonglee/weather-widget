e24421f89ab10dcb0bdc4868a734197be3f774db
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2024-12-27 11:40:13 +0800

    Getting data

--- app/Weather.tsx
+++ app/Weather.tsx
@@ -1,16 +1,22 @@
 "use client";
 import fetchJsonp from "fetch-jsonp";
-import { useState } from "react";
+// import { useState } from "react";
 export default function Weather() {
-  const [weather, setWeather] = useState();
-  // const today = new Date();
-  const url = "https://www.cts.com.tw/api/weather_list.json";
-  fetchJsonp(url, { jsonpCallback: "callback_weather" })
+  // const [weather, setWeather] = useState();
+  const today = new Date();
+  const url = `https://www.cts.com.tw/api/weather_list.json?t=${
+    today.getMonth() + 1
+  }${today.getDate()}`;
+  fetchJsonp(url, { jsonpCallbackFunction: "callback_weather" })
     .then(function (response) {
+      // console.log(response);
       return response.json();
     })
-    .then(function (response) {
-      setWeather(response);
+    .then(function (json) {
+      console.log("parsed json", json);
+    })
+    .catch(function (ex) {
+      console.log("parsing failed", ex);
     });
-  return <div className="flex justify-center">{weather}</div>;
+  return <div className="flex justify-center">weather</div>;
 }
