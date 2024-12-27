5e95525d989e5f254b94a375faee15274e0c898b
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2024-12-27 11:10:24 +0800

    jsonp

--- app/Weather.tsx
+++ app/Weather.tsx
@@ -1,17 +1,11 @@
 "use client";
-// import fetchJsonp from "fetch-jsonp";
+import fetchJsonp from "fetch-jsonp";
 import { useState } from "react";
 export default function Weather() {
   const [weather, setWeather] = useState();
   // const today = new Date();
   const url = "https://www.cts.com.tw/api/weather_list.json";
-  const options = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  fetch(url, options)
+  fetchJsonp(url, { jsonpCallbackFunction: "callback_weather" })
     .then(function (response) {
       return response.json();
     })
